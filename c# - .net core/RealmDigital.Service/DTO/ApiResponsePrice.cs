a4514171441cd6fd49ecd742babdf8f50dfc10ee
rickymster <32700103+rickymster@users.noreply.github.com>
2019-02-28 12:50:14 +0200

    Adding new class
    Moved class from controller

---
+++ c# - .net core/RealmDigital.Service/DTO/ApiResponsePrice.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealmDigital.Interview.Service.DTO
+{
+    public class ApiResponsePrice
+    {
+        public string SellingPrice { get; set; }
+        public string CurrencyCode { get; set; }
+    }
+}
---
+++ c# - .net core/RealmDigital.Service/DTO/ApiResponseProduct.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealmDigital.Interview.Service.DTO
+{
+    public class ApiResponseProduct
+    {
+        public string BarCode { get; set; }
+        public string ItemName { get; set; }
+        public List<ApiResponsePrice> PriceRecords { get; set; }
+    }
+}
